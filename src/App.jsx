6999f535c7775e1d9eb5004457644655e707ed19
Edwin <rugoogamuedwin5@gmail.com>
2023-11-30 22:00:44 +0300

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
-
-
   return (
     <>
-     
-        
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />}/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
